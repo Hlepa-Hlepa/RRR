5a29603384c504d6f9f39c475601dcebc05d34c7
Hlepa-Hlepa <kot101kot@inbox.ru>
2024-01-10 17:14:36 +0300

    Вывел в переменную (page - 1) * limit

--- react-mospolytech-2/src/Hooks/useUniversityData.tsx
+++ react-mospolytech-2/src/Hooks/useUniversityData.tsx
@@ -1,14 +1,7 @@
-<<<<<<<< HEAD:react-mospolytech-2/src/Hooks/useUniversityData.tsx
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import { DataType } from '../Interfaces/DataType';
 import { LIMIT_LIST_SCHOOL } from '../constants/constants';
-========
-import { useState, useEffect } from "react";
-import axios from "axios";
-import { DataType } from "../interfaces/IDataType";
-import { LIMIT_LIST_SCHOOL } from "../constants/constants";
->>>>>>>> styled:react-mospolytech-3/src/Hooks/useUniversityData.tsx
 
 export const useUniversityData = () => {
   const [dataSource, setDataSource] = useState<DataType[]>([]);
@@ -18,14 +11,12 @@ export const useUniversityData = () => {
   const getUniversity = async (page: number, limit: number) => {
     try {
       setLoading(true);
-      const response = await axios.get(
-        `http://universities.hipolabs.com/search?limit=${limit}&offset=${
-          (page - 1) * limit
-        }`
-      );
+      const offset = (page - 1) * limit;
+      const response = await axios.get(`http://universities.hipolabs.com/search?limit=${limit}&offset=${offset}`);
+      //const response = await axios.get(`http://universities.hipolabs.com/search?limit=${limit}&offset=${(page - 1) * limit}`);
       setDataSource(response.data);
     } catch (error) {
-      console.error(`Ошибка при получении информации: ${error}`);
+      console.error('Ошибка при получении информации:', error);
     } finally {
       setLoading(false);
     }
